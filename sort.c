7813a68a40bb23a2db273509842309d93c8d3321
mankestarkdev <mankestarkdev@gmail.com>
2022-11-30 23:01:42 +0100

    iniciando el swap

--- sort.c
+++ sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: papa <papa@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mankestarkdev <mankestarkdev@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 05:36:22 by papa              #+#    #+#             */
-/*   Updated: 2022/11/30 06:35:01 by papa             ###   ########.fr       */
+/*   Updated: 2022/11/30 22:59:57 by mankestarkd      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 
 
-static void push_swap (Nodo **pilaA, Nodo **pilaB, int taman_pila)
+static void push_swap(Nodo **pilaA, Nodo **pilaB, int taman_pila)
 {
     if (taman_pila == 2 && ! check_sorted(*pilaA))
         sa (pilaA);
@@ -22,4 +22,16 @@ static void push_swap (Nodo **pilaA, Nodo **pilaB, int taman_pila)
         peque_sort (pilaA);
     else if (taman_pila > 3 && !check_sorted(*pilaA))
         sort(pilaA, pilaB);
+}
+
+static void sort(Nodo *pila)
+{
+    int temp;
+
+    if (pila == NULL || pila->siguiente == NULL)
+        return (1);
+    temp = pila->numb;
+    pila->numb = pila->siguiente->numb;
+    pila->siguiente->numb = temp;
+
 }
