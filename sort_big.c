8070db1e3168963d3af4bc58c29764d62a0612a1
mankestarkdev <mankestarkdev@gmail.com>
2022-12-06 23:33:44 +0100

    vamos con el big_sort

--- sort_big.c
+++ sort_big.c
@@ -6,9 +6,35 @@
 /*   By: mankestarkdev <mankestarkdev@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 22:29:16 by mankestarkd       #+#    #+#             */
-/*   Updated: 2022/12/04 22:29:54 by mankestarkd      ###   ########.fr       */
+/*   Updated: 2022/12/06 23:28:45 by mankestarkd      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+static void	pushea_menos_tres(t_pila **pila_a, t_pila **pila_b)
+{
+	int	x;
+	int	j;
+	int	i;
+
+	x = pila_lenght(*pila_a);
+	j = 0;
+	i = 0;
+	while (x > 6 && i < x && i < j / 2)
+	{
+		if ((*pila_a)->index <= j / 2)
+		{
+			pb(pila_a, pila_b);
+			j++;
+		}
+		else
+			ra(pila_a);
+		i++;
+	}
+	while (x - j > 3)
+	{
+		pb(pila_a, pila_b);
+		j++;
+	}
+}
